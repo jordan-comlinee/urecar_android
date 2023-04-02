87c9efbe70be7d5e9cea7c97f92a53fce18f961c
jordan-comlinee <leesoyoung0112@gmail.com>
2023-04-02 15:32:12 +0900

    2023-04-02

--- app/src/main/java/kr/ac/duksung/parkingapp/LoginActivity.java
+++ app/src/main/java/kr/ac/duksung/parkingapp/LoginActivity.java
@@ -1,30 +1,35 @@
 package kr.ac.duksung.parkingapp;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    ActivityResultLauncher<Intent> launcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        moveHome(3);
-    }
+        Button loginButton = (Button) findViewById(R.id.loginButton);
 
 
-    private void moveHome(int sec) {
-        new Handler().postDelayed(new Runnable() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
+            public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
-                finish();
             }
-        }, 1000 * sec); //sec 초만큼 딜레이를 준 후 시작한다는 뜻
+        });
+
     }
 }

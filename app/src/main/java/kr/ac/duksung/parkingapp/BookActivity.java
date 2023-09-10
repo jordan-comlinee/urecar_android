7c367a3ecacbd812f80afe027c1a6b78e0c28232
seal3 <seal302@naver.com>
2023-09-10 14:48:42 +0900

    상단바 수정

--- app/src/main/java/kr/ac/duksung/parkingapp/BookActivity.java
+++ app/src/main/java/kr/ac/duksung/parkingapp/BookActivity.java
@@ -8,6 +8,7 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -52,8 +53,7 @@ public class BookActivity extends AppCompatActivity implements SwipeRefreshLayou
 
         TabLayout tab = findViewById(R.id.tab);
         tab.setupWithViewPager(vp);
-
-
+        tab.setTabTextColors(Color.rgb(255,255,255),Color.rgb(255,255,255));
         // 위로 끌어올리면 새로고침되도록 구현하였음
         swipeRefreshLayout = findViewById(R.id.bookLayout);
         swipeRefreshLayout.setOnRefreshListener(this);
